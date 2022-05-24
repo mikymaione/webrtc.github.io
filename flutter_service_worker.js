997ec9492e8a3d3d7d7fb8ce4e81b15d7a1d8f8c
Michele Maione <mikymaione@hotmail.it>
2022-05-24 23:36:14 +0200

    release

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -13,8 +13,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "29f0843c642c4d46c431404900ec936e",
-"/": "29f0843c642c4d46c431404900ec936e",
+"index.html": "a3637ce72e1811f61a06d05ba0c8ca40",
+"/": "a3637ce72e1811f61a06d05ba0c8ca40",
 "main.dart.js": "7adf6ba90ab039bafc0e2c2a0d66e6ff",
 "manifest.json": "1515744a4734f8534410baa862ac4b73",
 "version.json": "38844fb7225fd2b67ceff3c951cd9b62"
